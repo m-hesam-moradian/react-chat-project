ea8be2443a1bd01f17fcc4ac1f8d24c4c28fc57f
hesam <m.hesam.moradian@gmail.com>
2023-09-26 20:22:58 +0330

    style almost done

--- src/components/chatbar/TypeInput.jsx
+++ src/components/chatbar/TypeInput.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { IoAttachSharp } from "react-icons/io";
+import { IoMdAttach } from "react-icons/io";
+import { LuImagePlus } from "react-icons/lu";
+import { BiSolidSend } from "react-icons/bi";
+
 
 const TypeInput = () => {
   return (
     <div className="TypeInput">
-      <input type="text" />
+      <input className="text-input" type="text" placeholder="type here..." />
       <div className="input-icons">
-        <GrAttachment></GrAttachment>
-        <button>send</button>
+        <input className="d-none" type="file" id="file-message-send" />
+        <label htmlFor="file-message-send">
+          <IoMdAttach />
+        </label>
+        <input className="d-none" type="file" id="media-message-send" />
+
+        <label htmlFor="file-message-send">
+          <LuImagePlus />
+        </label>
+
+        <button>send <BiSolidSend></BiSolidSend></button>
       </div>
     </div>
   );
