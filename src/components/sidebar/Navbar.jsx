d8f5146ba8a223e19f9f833403588fbc3b5547e9
hesam <m.hesam.moradian@gmail.com>
2023-09-26 09:19:08 +0330

    finish side bar

--- src/components/sidebar/Navbar.jsx
+++ src/components/sidebar/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-
+import { BsSearch } from "react-icons/bs";
 const Navbar = () => {
   return (
     <>
       <div className="Navbar">
         <h3 className="nav-logo">react chat</h3>
+        <label className="search-icon" htmlFor="search">
+          <BsSearch></BsSearch>
+        </label>
         <div className="profile">
           <div className="acount">
             <img className="profile-img" src="/images/me.png" alt="" />
             {/* <div className="nav-name">hesam</div> */}
+            <span>hesam</span>
           </div>
           <button className="btn btn-secondary nav-btn ">Log out</button>
         </div>
--- src/components/sidebar/Searchbar.jsx
+++ src/components/sidebar/Searchbar.jsx
@@ -4,19 +4,12 @@ const Searchbar = () => {
   return (
     <div>
       <form className="search-form">
-        <input
+        <input id="search"
           className="search-input"
           type="text"
           placeholder="search someone"
         />
-        <div className="search-result-user">
-          <img className="profile-img" src="/images/me.png" alt="" />
-          hesam moradian
-        </div>
-        <div className="search-result-user">
-          <img className="profile-img" src="/images/me.png" alt="" />
-          hesam moradian
-        </div>
+
       </form>
     </div>
   );
