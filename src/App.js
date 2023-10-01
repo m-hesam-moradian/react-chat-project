92bf506063b0f6e87e482b2f8e7621f041908279
hesam <m.hesam.moradian@gmail.com>
2023-10-01 18:58:42 +0330

    clean

--- src/App.js
+++ src/App.js
@@ -13,18 +13,6 @@ export default function App() {
   };
   return (
     <div>
-      {/* <form action="/action_page.php" onSubmit={handleSubmit}>
-        <label for="fname">First name:</label>
-        <br />
-        <input type="text" id="fname" name="fname" value="John" />
-        <br />
-        <label for="lname">Last name:</label>
-        <br />
-        <input type="text" id="lname" name="lname" value="Doe" />
-        <br />
-        <br />
-        <input type="submit" value="Submit" />
-      </form> */}
       <Register />
     </div>
   );
