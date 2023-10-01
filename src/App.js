e52cd3c46da01f95d7b39f7e984c5a75aeea7bd1
hesam <m.hesam.moradian@gmail.com>
2023-10-01 18:58:23 +0330

    done firebase error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Home from "./pages/Home";
-import Register from "./pages/Register"
+import Register from "./pages/Register";
 import "./pages/pageStyle.css";
 import "./mainStyle.css";
 import "./components/componentsStyle.css";
 
-
 export default function App() {
-  return <Register />;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form data:");
+    // Perform any necessary actions, such as sending the form data to an API or updating the state of the component
+  };
+  return (
+    <div>
+      {/* <form action="/action_page.php" onSubmit={handleSubmit}>
+        <label for="fname">First name:</label>
+        <br />
+        <input type="text" id="fname" name="fname" value="John" />
+        <br />
+        <label for="lname">Last name:</label>
+        <br />
+        <input type="text" id="lname" name="lname" value="Doe" />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form> */}
+      <Register />
+    </div>
+  );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,61 @@
-import { react } from "fontawesome";
+import { react } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebase";
+import { useState } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
+  const [err, serErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          serErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid:res.user.uid,
+              displayName,
+              email,
+              photoURL:downloadURL
+            });
+          });
+        }
+      );
+    } catch (err) {
+      serErr(true);
+      console.log(err);
+    }
+  };
   return (
     <div className="form-container">
       <div className="form-box">
         <h2 className="logo">react chat</h2>
         <p>made by M.Hesam.Moradian</p>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
@@ -16,6 +65,7 @@ const Register = () => {
             here
           </label>
           <button type="submit">sing up</button>
+          {err && <span>somthing went wrong</span>}
         </form>
         <p>
           you do have an acount? <span className="login-link">login</span>
