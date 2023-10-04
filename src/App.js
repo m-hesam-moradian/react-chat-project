c07fa06b3b25b17981008c2b427685c9edbaf4fa
hesam <m.hesam.moradian@gmail.com>
2023-10-04 17:13:56 +0330

    .

--- src/App.js
+++ src/App.js
@@ -5,24 +5,34 @@ import Login from "./pages/Login";
 import "./pages/pageStyle.css";
 import "./mainStyle.css";
 import "./components/componentsStyle.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/authContext";
 
 export default function App() {
-  const { currentUser } = useContext(AuthContext)
-  console.log(currentUser);
+  const { currentUser } = useContext(AuthContext);
+  // console.log(currentUser);
+
+  const ProteceRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+  };
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
-            {/* <Route path="*" element={<NoPage />} /> */}
-          </Route>
-          <Route path="/home" element={<Home />}>
-          </Route>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <Route
+            index
+            element={
+              <ProteceRoute>
+                <Home />
+              </ProteceRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
