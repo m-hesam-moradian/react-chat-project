61c138541cae353ee1e13308e7398eede8612041
hesam <m.hesam.moradian@gmail.com>
2023-10-04 17:14:36 +0330

    login done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,29 @@
-import { react } from "fontawesome";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-const login = () => {
+const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/login");
+    } catch (err) {
+      setErr(true);
+    }
+  };
   return (
     <div className="form-container">
       <div className="form-box">
         <h2 className="logo">react chat</h2>
         <p>made by M.Hesam.Moradian</p>
-        <form action="">
+        <form onSubmit={handleSubmit} action="">
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <button type="submit">sing in</button>
@@ -18,4 +35,4 @@ const login = () => {
     </div>
   );
 };
-export default login;
+export default Login;
