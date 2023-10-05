d28d2700fd6ef9392155c7b7178f115844da0e77
hesam <m.hesam.moradian@gmail.com>
2023-10-05 15:16:24 +0330

    select user

--- src/components/sidebar/Searchbar.jsx
+++ src/components/sidebar/Searchbar.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-import { db } from "../../firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {  db } from "../../firebase";
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 const Searchbar = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState("");
   const [err, setErr] = useState("");
+  const [currentUser]=useContext(AuthContext)
 
   const handleSearch = async (e) => {
-    const q = query(
+    const q = +query(
       collection(db, "users"),
       where("displayName", "==", username)
     );
+
     try {
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         setUser(doc.data());
-        console.log(doc.data);
+        console.log(user);
       });
     } catch (err) {
       setErr(true);
@@ -26,6 +30,18 @@ const Searchbar = () => {
     e.code === "Enter" && handleSearch();
   };
 
+  const handleSelect =async () => {
+const combinedId=currentUser.uid>user.uid?currentUser.uid+user.uid:user.uid+currentUser.uid
+    try {
+  
+      const res = await getDocs(db, "chats", combinedId);
+      if (!res.exists()) {
+        await setDoc(doc,(db,'chats',combinedId),{messages:[]})
+      }
+    } catch (err) {
+      
+     }
+ }
   return (
     <div>
       <form
@@ -43,14 +59,21 @@ const Searchbar = () => {
           placeholder="search someone"
         />
       </form>
-      {err && <span> User not found!</span>}
-      <div className="Contacts">
-        {user && (
+      {err && (
+        <div className="Contacts ">
           <div className="Contacts-user">
+            <div className="user-info">
+              <span className="User-name">User not found!</span>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="Contacts search-contacts">
+        {user && (
+          <div onClick={handleSelect} className="Contacts-user">
             <img className="profile-img" src={user.photoURL} alt="" />
             <div className="user-info">
               <span className="User-name">{user.displayName}</span>
-              <span className="user-lastword">see you</span>
             </div>
           </div>
         )}
