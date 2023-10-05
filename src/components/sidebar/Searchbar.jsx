7912069acd80e7620d6473e3b42f2b3291e328bf
hesam <m.hesam.moradian@gmail.com>
2023-10-05 14:24:30 +0330

    search

--- src/components/sidebar/Searchbar.jsx
+++ src/components/sidebar/Searchbar.jsx
@@ -1,45 +1,59 @@
 import React, { useState } from "react";
-import {db} from "../fire"
+import { db } from "../../firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const Searchbar = () => {
-  const [username, setUsername] = useState('')
-  const [user, setUser] = useState('')
-  const [err, setErr] = useState('')
-  
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+  const [err, setErr] = useState("");
 
+  const handleSearch = async (e) => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        console.log(doc.data);
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  const handlekey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
 
-
-
-  const handleSearch = () =>}
-
-
-
-
-  const handlekey = e => {
-    e.code === 'Enter' && handleSearch()
-  }
   return (
     <div>
-      <form className="search-form">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="search-form"
+      >
         <input
           onKeyDown={handlekey}
-          onChange={e=>setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value)}
           id="search"
           className="search-input"
           type="text"
           placeholder="search someone"
         />
       </form>
+      {err && <span> User not found!</span>}
       <div className="Contacts">
-      
-        <div className="Contacts-user">
-          <img className="profile-img" src="/images/me.png" alt="" />
-          <div className="user-info">
-            <span className="User-name">hesam moradian</span>
-            <span className="user-lastword">see you</span>
+        {user && (
+          <div className="Contacts-user">
+            <img className="profile-img" src={user.photoURL} alt="" />
+            <div className="user-info">
+              <span className="User-name">{user.displayName}</span>
+              <span className="user-lastword">see you</span>
+            </div>
           </div>
-        </div>
-
+        )}
       </div>
     </div>
   );
